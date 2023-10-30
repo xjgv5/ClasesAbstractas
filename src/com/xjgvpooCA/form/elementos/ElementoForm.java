e5a8650566e376a8d0f6408368caee4365902322
Javier Gomez <javier.gov@outlook.es>
2023-10-29 22:33:48 -0600

    Clase elemento form

--- src/com/xjgvpooCA/form/elementos/ElementoForm.java
+++ src/com/xjgvpooCA/form/elementos/ElementoForm.java
@@ -2,5 +2,20 @@ package com.xjgvpooCA.form.elementos;
 
 abstract public class ElementoForm {
 
+    protected String valor;
+    protected String nombre;
+
+    public ElementoForm() {
+    }
+
+    public ElementoForm(String nombre) {
+        this();
+        this.nombre = nombre;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
     public abstract String dibujarHTML();
 }
