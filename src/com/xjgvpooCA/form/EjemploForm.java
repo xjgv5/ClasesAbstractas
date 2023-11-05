13341dd157c51a9d6c5db997d3ce519f0c0226ec
Javier Gomez <javier.gov@outlook.es>
2023-11-05 11:56:00 -0600

    Optimizacion de código

--- src/com/xjgvpooCA/form/EjemploForm.java
+++ src/com/xjgvpooCA/form/EjemploForm.java
@@ -1,9 +1,14 @@
 package com.xjgvpooCA.form;
 
+import com.xjgvpooCA.form.elementos.ElementoForm;
 import com.xjgvpooCA.form.elementos.InputForm;
 import com.xjgvpooCA.form.elementos.SelectForm;
 import com.xjgvpooCA.form.elementos.TextAreaForm;
 import com.xjgvpooCA.form.elementos.select.Opcion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class EjemploForm {
     public static void main(String[] args) {
@@ -16,17 +21,31 @@ public class EjemploForm {
 
         SelectForm lenguaje = new SelectForm("lenguaje");
         Opcion java = new Opcion("1", "Java");
-        lenguaje.addOpcion(java);
-        lenguaje.addOpcion(new Opcion("2", "Python"));
-        lenguaje.addOpcion(new Opcion("3", "Javascript"));
-        lenguaje.addOpcion(new Opcion("4", "Typescript"));
-        lenguaje.addOpcion(new Opcion("5", "PHP"));
+        lenguaje.addOpcion(java)
+            .addOpcion(new Opcion("2", "Python"))
+            .addOpcion(new Opcion("3", "Javascript"))
+            .addOpcion(new Opcion("4", "Typescript"))
+            .addOpcion(new Opcion("5", "PHP"));
 
         username.setValor("john.doe");
         password.setValor("a1b3x2");
         email.setValor("johnd.email.com");
         edad.setValor("24");
+        experiencia.setValor("Mas de 10 años de experiencia...");
         java.setSelected(true);
+
+        List<ElementoForm> elementos =  Arrays.asList(username,
+                password,
+                email,
+                edad,
+                experiencia,
+                lenguaje);
+
+
+        elementos.forEach(e -> {
+            System.out.println(e.dibujarHTML());
+            System.out.println("<br>");
+        });
 
     }
 }
