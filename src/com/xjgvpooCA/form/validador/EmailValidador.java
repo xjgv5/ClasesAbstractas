f143a6277fb282f788a5d155d8676de8cd2bd9ef
Javier Gomez <javier.gov@outlook.es>
2023-11-05 21:47:11 -0600

    validador de email

---
+++ src/com/xjgvpooCA/form/validador/EmailValidador.java
@@ -0,0 +1,21 @@
+package com.xjgvpooCA.form.validador;
+
+public class EmailValidador extends Validador{
+
+    protected String mensaje = " el formato del email es inválido";
+    private final static  String EMAIL_REGEX = "^(.+)@(.)$";
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        return valor.matches(EMAIL_REGEX);
+    }
+}
